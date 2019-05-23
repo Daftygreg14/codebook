cdf0c930342680493248848fea8aa296b3a28f53
Argonus <argonuspiotr@gmail.com>
2019-05-23 07:41:58 +0200

    Finish chapter 12

--- erlang/learn_you_some_erlang_for_great_good/lib/chapter_12/linkmon.erl
+++ erlang/learn_you_some_erlang_for_great_good/lib/chapter_12/linkmon.erl
@@ -1,13 +1,53 @@
-%%%-------------------------------------------------------------------
-%%% @author piotr
-%%% @copyright (C) 2019, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 22. May 2019 6:36 PM
-%%%-------------------------------------------------------------------
 -module(linkmon).
 -author("piotr").
 
 %% API
--export([]).
+-compile(export_all).
+
+chain(0) ->
+  receive
+    X -> X
+  after 2000 ->
+    exit(normal)
+  end;
+chain(N) when N > 0 ->
+  process_flag(trap_exit, true),
+  spawn_link(fun() -> chain(N - 1) end),
+  receive
+    X -> X
+  end.
+
+monitor_chain(N) ->
+  spawn_monitor(fun() -> chain(N) end).
+
+start_critic() ->
+  spawn(?MODULE, restarter, []).
+
+judge(Band, Album) ->
+  Ref = make_ref(),
+  critic ! {self(), Ref, {Band, Album}},
+  receive
+    {Ref, Criticism} -> Criticism
+  after 2000 ->
+    timeout
+  end.
+
+critic() ->
+  receive
+    {From, Ref, {"Rage Against", "Unify"}} -> {From ! {Ref, "They are great"}};
+    {From, Ref, {"System Down", "Memoize"}} -> {From ! {Ref, "They are not great"}}
+  end,
+  critic().
+
+restarter() ->
+  process_flag(trap_exit, true),
+  Pid = spawn_link(?MODULE, critic, []),
+  register(critic, Pid),
+  receive
+    {'EXIT', Pid, normal} -> ok;
+    {'EXIT', Pid, shutdown} -> ok;
+    {'EXIT', Pid, _} -> restarter()
+  end.
+
+murder_critic(Pid) ->
+  exit(Pid, headshot).
