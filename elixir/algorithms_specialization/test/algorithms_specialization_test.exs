a97e63f73373210c1a0d1a57f365643770b92e51
Argonus <argonuspiotr@gmail.com>
2022-03-02 12:00:51 +0100

    Quite a lot of stuff

--- elixir/algorithms_specialization/test/algorithms_specialization_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule AlgorithmsSpecializationTest do
-  use ExUnit.Case
-  doctest AlgorithmsSpecialization
-
-  test "greets the world" do
-    assert AlgorithmsSpecialization.hello() == :world
-  end
-end
--- elixir/grokking_algorithms/lib/grokking_algorithms/chapter_10/recommendation_engine.ex
+++ elixir/grokking_algorithms/lib/grokking_algorithms/chapter_10/recommendation_engine.ex
@@ -9,22 +9,33 @@ defmodule GrokkingAlgorithms.Chapter10.RecommendationEngine do
     morpheus: %{comedy: 2, action: 5, drama: 1, horror: 3, romance: 1}
   }
 
-  @type movies :: %{comendy: integer, action: integer, drama: integer, horror: integer, romance: integer}
+  @type movies :: %{
+          comendy: integer,
+          action: integer,
+          drama: integer,
+          horror: integer,
+          romance: integer
+        }
   @type person :: {atom, movies}
 
   @spec find_nearest(person) :: person
   def find_nearest({input_name, input_points}) do
     @values
     |> Enum.map(fn {reference_name, reference_points} ->
-      points = Enum.reduce(@movie_types, 0.0, fn type, acc ->
-        a1 = Map.fetch!(input_points, type)
-        a2 = Map.fetch!(reference_points, type)
-        acc + :math.pow((a1 * a1) - (a2 * a2), 2)
-      end)
+      points =
+        Enum.reduce(@movie_types, 0.0, fn type, acc ->
+          a1 = Map.fetch!(input_points, type)
+          a2 = Map.fetch!(reference_points, type)
+          acc + :math.pow(a1 * a1 - a2 * a2, 2)
+        end)
+
       {reference_name, :math.sqrt(points)}
     end)
-    |> Enum.sort_by(fn {_name, points} ->
-      points
-    end, :desc)
+    |> Enum.sort_by(
+      fn {_name, points} ->
+        points
+      end,
+      :desc
+    )
   end
 end
