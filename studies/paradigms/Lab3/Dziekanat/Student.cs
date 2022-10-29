976abbfd25377abaa048e6056436e3ac510cd50a
Piotr Rybarczyk <argonuspiotr@gmail.com>
2022-10-29 13:00:30 +0200

    Finish Lab 1

--- studies/paradigms/Lab3/Dziekanat/Student.cs
+++ studies/paradigms/Lab3/Dziekanat/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace Dziekanat
 {
     internal class Student
     {
+        public string Imie, Nazwisko, Grupa, Indeks;
+
+        public Student(string imie = "", string nazwisko = "", string grupa = "", string indeks = "")
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Grupa = grupa;   
+            Indeks = indeks;
+        }
+
+        public void Zapisz()
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Dziekanat";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            string[] dane = new string[] { Imie, Nazwisko, Grupa, Indeks };
+            File.WriteAllLines($@"{folder}\{Nazwisko}_{Indeks}.txt", dane);
+        }
     }
 }
--- studies/paradigms/Lab3/Dziekanat/StudentManager.cs
+++ studies/paradigms/Lab3/Dziekanat/StudentManager.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using Rybarczyk.UiTools;
 
 namespace Dziekanat
 {
     internal class StudentManager
     {
+        private List<Student> studenci;
+
+        public StudentManager()
+        {
+            studenci = new List<Student>(4);
+        }
+
+        private void LadujDane()
+        {
+            studenci.Add(new Student("Piotr", "Rybarczyk", "Z304", "10117"));
+            studenci.Add(new Student("Adam", "Lepkowski", "Z303", "10116"));
+            studenci.Add(new Student("Krzysztof", "Budzynski", "Z302", "10115"));
+            studenci.Add(new Student("Tomasz", "Dudziec", "Z301", "10114"));
+        }
+
+        public int ListaStudentow()
+        {
+            Console.Clear();
+            Menu lista = new Menu();
+            string[] dane = new string[studenci.Count()];
+            for(int i = 0; i < dane.Length; i++)
+            {
+                Student s = studenci[i];
+                dane[i] = $"{s.Imie} {s.Nazwisko} {s.Grupa} {s.Indeks}";
+            }
+            lista.Konfiguruj(dane);
+            return 0;
+        }
+
+        public void EdytujStudenta()
+        {
+
+        }
+
+        public void DodajStudenta()
+        {
+            Student newStudent = new Student();
+            Console.Clear();
+            Console.WriteLine("********** Dodawanie Studenta *********");
+            Console.WriteLine("Imie            :"); newStudent.Imie = Console.ReadLine();
+            Console.WriteLine("Nazwisko        :"); newStudent.Nazwisko = Console.ReadLine();
+            Console.WriteLine("Grupa           :"); newStudent.Grupa = Console.ReadLine();
+            Console.WriteLine("Index           :"); newStudent.Indeks = Console.ReadLine();
+        }
+
+        internal void ZapiszZmiany()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
