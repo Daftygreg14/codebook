96ee9fe96434e4d1431e5360aa59f06e8a564299
Piotr Rybarczyk <argonuspiotr@gmail.com>
2022-11-08 19:08:54 +0100

    ASD13

---
+++ studies/algorithms-and-data-structures/spoj-2022/spoj-2022/spoj-2022.cpp
@@ -0,0 +1,10 @@
+﻿// spoj-2022.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include "asd_1_3.h";
+
+int main()
+{
+    run_asd_1_3();
+    return 0;
+}
