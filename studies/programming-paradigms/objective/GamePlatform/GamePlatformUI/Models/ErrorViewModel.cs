9a8c6c472859822a46c070797ca70c1feaa4fb32
Piotr Rybarczyk <argonuspiotr@gmail.com>
2023-05-13 19:14:52 +0200

    Init Commit
    Init Commit
    
    Disable auth for now
    
    Add DB Connection
    
    s

---
+++ studies/programming-paradigms/objective/GamePlatform/GamePlatformUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamePlatformUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
