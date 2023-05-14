4fef8b770c966ced6c63deca239b5e03c7835286
Piotr Rybarczyk <argonuspiotr@gmail.com>
2023-05-14 20:06:42 +0200

    Setup Identity

--- studies/programming-paradigms/objective/GamePlatform/GamePlatformUI/Program.cs
+++ studies/programming-paradigms/objective/GamePlatform/GamePlatformUI/Program.cs
@@ -1,5 +1,7 @@
-using GamePlatformUI.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using GamePlatformUI.Areas.Identity.Data;
+using GamePlatformUI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
@@ -7,6 +9,13 @@ string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRI
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.
+    AddDefaultIdentity<User>(options => {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+    }).
+    AddEntityFrameworkStores<ApplicationDbContext>();
 
 
 var app = builder.Build();
@@ -19,11 +28,11 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
---
+++ studies/programming-paradigms/objective/GamePlatform/Readme.md
@@ -0,0 +1,15 @@
+# Game Platform
+
+## Modules
+### Game Platform UI
+ASP.NET Core MVC application that provides the user interface for the Game Platform.
+It is a simple web application that uses bootstrap for styling html elements.
+
+Additionaly it uses the following core libraries:
+- Microsoft.AspNetCore.Identity.EntityFrameworkCore - for user management. It provides the user registration and login functionality.
+- Microsoft.EntityFrameworkCore.SqlServer - for database access. It provides the functionality to access the database.
+- Microsoft.EntityFrameworkCore.Tools - for database migrations. It provides the functionality to create and update the database schema.
+- Microsoft.VisualStudio.Web.CodeGeneration.Design - for scaffolding. I just hate to write boilerplate code.
+
+
+## Database
